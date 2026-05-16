ffca21239f242b8258659b8962e7c41e647f28f4
a7mad-r7l <ahmadalrahal341@gmail.com>
2026-05-16 23:58:06 +0300

    Resolve merge conflicts in constants and pubspec

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
 import 'package:kidcare/views/forgot_password_view.dart';
 
 // الواجهات الأساسية
 import 'package:kidcare/views/main_advanced.dart';
 import 'package:kidcare/views/login_view.dart';
 import 'package:kidcare/views/sign_up_view.dart';
-import 'package:kidcare/views/homeView.dart'; // ✅ إضافة
+import 'package:kidcare/views/homeView.dart';
 
 // Sign Up
 import 'package:kidcare/controllers/sign_up_controller.dart';
@@ -23,9 +24,22 @@ import 'package:kidcare/views/verify_otp_view.dart';
 import 'package:kidcare/controllers/verify_otp_controller.dart';
 import 'package:kidcare/core/repos/verify_otp_repo.dart';
 
+//  واجهات الدفع الجديدة
+
+import 'package:kidcare/views/payment/payment_method_view.dart';
+import 'package:kidcare/views/payment/checkout_summary_view.dart';
+import 'package:kidcare/views/payment/payment_success_view.dart';
+
 import 'controllers/forgot_password_controller.dart';
 
-void main() {
+void main() async {
+  //  لتهيئة فلاتر قبل تشغيل أي ميزة Native مثل Stripe
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // ✅ ضع مفتاح Stripe الخاص بك هنا (Publishable Key الذي يبدأ بـ pk_test)
+
+  Stripe.publishableKey = 'pk_test_';
+
   runApp(const MyApp());
 }
 
@@ -42,7 +56,8 @@ class MyApp extends StatelessWidget {
         scaffoldBackgroundColor: Colors.white,
         fontFamily: 'Roboto',
       ),
-      initialRoute: '/',
+      // initialRoute: '/',
+      initialRoute: '/payment-method',
       getPages: [
         GetPage(name: '/', page: () => const PediatricClinicScreen()),
 
@@ -95,6 +110,19 @@ class MyApp extends StatelessWidget {
                   () => ForgotPasswordController(),
             );
           }),
+        ),
+        //  Payment Routes
+        GetPage(
+          name: '/payment-method',
+          page: () => const PaymentMethodView(),
+        ),
+        GetPage(
+          name: '/checkout-summary',
+          page: () => const CheckoutSummaryView(),
+        ),
+        GetPage(
+          name: '/payment-success',
+          page: () => const PaymentSuccessView(),
         ),
       ],
     );
